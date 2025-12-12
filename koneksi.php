adf561a1ff24b64a3b22eaa5618912d04dd4a1d6
naswa10 <naswasyarira33@gmail.com>
2025-12-12 09:47:48 +0700

    Koneksi Database

---
+++ koneksi.php
@@ -0,0 +1,13 @@
+<?php
+$host = "localhost";  // Alamat server database (biasanya localhost)
+$username = "root";   // Username untuk login ke database
+$password = "";       // Password unutk login (biassnya kosong untuk localhost)
+$dbname = "db_penjualann";
+
+$conn = new mysqli($host, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Koneksi gagal: " . $conn->connect_error);
+}
+echo "Koneksi berhasil";
+?>
